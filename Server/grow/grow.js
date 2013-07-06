bbbe67705f9f145f83983679a21ed6870df0e412
System Administrator <root@Benjamins-Mac-mini.local>
2013-07-06 15:14:18 -0700

    Initial commit

---
+++ Server/grow/grow.js
@@ -0,0 +1,13 @@
+module.exports = function Grow()
+{
+  var dt = 0;
+  var grid = new Array();
+  for(var i = 0; i < 10; ++i)
+  {
+    grid[i] = new Array();
+    for(var j = 0; j < 10; ++j)
+    {
+      grid[i][j] = false;
+    }
+  }
+}

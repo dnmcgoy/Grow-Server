c7e36c1956983bb432ef6be812c6b340a3033b56
Donald McGaughey <dnmcgoy@gmail.com>
2013-07-07 02:08:15 -0700

    Cleanup.

--- Server/public/growSocket.js
+++ Server/public/growSocket.js
@@ -2,8 +2,3 @@ var socket = io.connect();
 socket.on('plantSuccessful', function (data) {
     $('#intro').append(data);
 });
-
-socket.on("treeCreated", function(data) {
-    $('#intro').append("Something");
-    $('#frame').append($("#drag1").clone());
-});

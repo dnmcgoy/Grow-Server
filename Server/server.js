45b3c9edddf5db8180bfdc0b7fa66a8e6ea594e5
Donald McGaughey <dnmcgoy@gmail.com>
2013-07-06 19:15:34 -0700

    Change ports.

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ var app = require('express')()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
-server.listen(80);
+server.listen(8000);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
